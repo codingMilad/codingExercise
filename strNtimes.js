ccc30fa1684c9b6abbea0079f68a7ea97f67d762
codingMilad <m3loo@yahoo.com>
2021-10-24 11:49:10 +0200

    Create strNtimes.js

---
+++ strNtimes.js
@@ -0,0 +1,10 @@
+function repeatStr(n, s) {
+  let str = "";
+  for (let i = 1; i <= n; i++) {
+    str += s;
+  }
+  return str;
+}
+
+console.log(repeatStr(3, "*"), "***");
+console.log(repeatStr(5, "#"), "#####");
